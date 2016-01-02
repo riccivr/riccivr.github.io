af956a69c1d4878275e1e7ae8bcae6f0da17299b
Ricardo Veronese Ricci <ricci.vr@gmail.com>
2016-01-01 21:59:31 -0430

    initial commit

---
+++ main.js
@@ -0,0 +1,71 @@
+var doughnutData = [
+    {
+        value: 80,
+        color:"#1abc9c"
+    },
+    {
+        value : 20,
+        color : "#ecf0f1"
+    }
+];
+var myDoughnut = new Chart(document.getElementById("javascript").getContext("2d")).Doughnut(doughnutData);
+
+var doughnutData = [
+    {
+        value: 90,
+        color:"#1abc9c"
+    },
+    {
+        value : 10,
+        color : "#ecf0f1"
+    }
+];
+var myDoughnut = new Chart(document.getElementById("bootstrap").getContext("2d")).Doughnut(doughnutData);
+
+var doughnutData = [
+    {
+        value: 90,
+        color:"#1abc9c"
+    },
+    {
+        value : 10,
+        color : "#ecf0f1"
+    }
+];
+var myDoughnut = new Chart(document.getElementById("wordpress").getContext("2d")).Doughnut(doughnutData);
+
+var doughnutData = [
+    {
+        value: 100,
+        color:"#1abc9c"
+    },
+    {
+        value : 0,
+        color : "#ecf0f1"
+    }
+];
+var myDoughnut = new Chart(document.getElementById("html").getContext("2d")).Doughnut(doughnutData);
+
+var doughnutData = [
+    {
+        value: 60,
+        color:"#1abc9c"
+    },
+    {
+        value : 40,
+        color : "#ecf0f1"
+    }
+];
+var myDoughnut = new Chart(document.getElementById("photoshop").getContext("2d")).Doughnut(doughnutData);
+                            
+var doughnutData = [
+    {
+        value: 50,
+        color:"#1abc9c"
+    },
+    {
+        value : 50,
+        color : "#ecf0f1"
+    }
+];
+var myDoughnut = new Chart(document.getElementById("illustrator").getContext("2d")).Doughnut(doughnutData);
